864a2274e62302d22ba156e7d112325989ac5d87
Timothy Lindsay <zodd@Timothys-MBP.lan>
2020-04-27 17:25:24 -0700

    Updated single quote and print width by adding prettier config file

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-function getAllRepos(searchRepo){
-  const url = `https://api.github.com/users/${searchRepo}/repos`
+function getAllRepos(searchRepo) {
+  const url = `https://api.github.com/users/${searchRepo}/repos`;
 
   fetch(url)
-    .then(response => {
-      if (response.ok){
+    .then((response) => {
+      if (response.ok) {
         return response.json();
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
+    .then((responseJson) => displayResults(responseJson))
+    .catch((err) => {
       $('#js-error-message').text(`Something went wrong with your request: ${err.message}`);
     });
 }
 
-
-function displayResults(responseJson){
+function displayResults(responseJson) {
   $('.results').empty();
-  responseJson.forEach(function(repo){
-    $('.results').append(`<li><a href="${repo.owner.url}" target="_blank">${repo.name}</a></li>`)
+  responseJson.forEach(function (repo) {
+    $('.results').append(`<li><a href="${repo.owner.url}" target="_blank">${repo.name}</a></li>`);
     $('#result-list').removeClass('hidden');
   });
 }
 
-function watchSubmit(){
-  $('form').submit(event=> {
+function watchSubmit() {
+  $('form').submit((event) => {
     event.preventDefault();
     const searchRepo = $('#js-search-form').val();
     getAllRepos(searchRepo);
